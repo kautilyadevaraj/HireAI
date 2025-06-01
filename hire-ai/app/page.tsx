aa812f40a30bbc2aacbe1d4dba1512d655549bfb
Pushan T <pushan.sahanaganesh@gmail.com>
2025-06-02 01:46:39 +0530

    feat: redirect based on user role

--- hire-ai/app/page.tsx
+++ hire-ai/app/page.tsx
@@ -1,1 +1,40 @@
-export default function Page() {return <h1>dashboard</h1> }
+import { createClient } from "@/utils/supabase/server";
+import { jwtDecode, JwtPayload } from "jwt-decode";
+import { redirect } from "next/navigation";
+
+interface CustomJwtPayload extends JwtPayload {
+    user_role: string;
+}
+
+export default async function RootPage() {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data.user) {
+        redirect("/login");
+    }
+
+    const getUserRole = async () => {
+        const { data, error } = await supabase.auth.getSession();
+        if (error || !data.session) {
+            redirect("/login");
+            return null;
+        }
+        const jwt = data.session!.access_token;
+        const decoded = jwtDecode<CustomJwtPayload>(jwt);
+        const user_role: string = decoded.user_role;
+        return user_role;
+    };
+
+    if ((await getUserRole()) == "recruiter") {
+        redirect("/dashboard");
+    }
+
+    return (
+        <div className="flex min-h-screen w-full items-center justify-center">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4">Welcome to Hire AI</h1>
+                <p className="text-lg text-gray-600">You shouldn't be here</p>
+            </div>
+        </div>
+    );
+}
