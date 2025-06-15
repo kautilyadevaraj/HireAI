9df43d8fe79c276e3ff7ec0fd98673c4a5801b75
Pushan T <pushan.sahanaganesh@gmail.com>
2025-06-16 01:28:58 +0530

    chore: cleanup code and solve build errors

--- hire-ai/app/setup/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { revalidatePath } from "next/cache";
-
-export async function revalidateJugad() {
-    //idk why this isn't working i will cry
-    revalidatePath("/", "layout");
-}
--- hire-ai/app/setup/setup-form.tsx
+++ hire-ai/app/setup/setup-form.tsx
@@ -7,8 +7,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { createClient } from "@/utils/supabase/client";
-import { revalidateJugad } from "./actions";
-
 export default function SetupForm() {
     const [fullName, setFullName] = useState("");
     const [role, setRole] = useState("");
